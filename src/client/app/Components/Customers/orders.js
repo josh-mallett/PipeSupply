52b1bc600cf5ca33f8d85105cc8567c7a18dca7a
Josh <josh-mallett@outlook.com>
2018-04-22 15:01:49 -0500

    changing to laptop
    :wq

--- src/client/app/Components/Customers/orders.js
+++ src/client/app/Components/Customers/orders.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Customer extends Component {
+export default class Orders extends Component {
   constructor (props) {
     super(props);
 
--- src/client/app/Components/Customers/shop.js
+++ src/client/app/Components/Customers/shop.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Customer extends Component {
+export default class Shop extends Component {
   constructor (props) {
     super(props);
 
